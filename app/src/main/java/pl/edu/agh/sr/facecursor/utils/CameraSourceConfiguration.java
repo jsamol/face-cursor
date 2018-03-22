917f3f0cbbee2130ed8ee13fc9fb598733e9893b
Julia Samól <juliasamol@Cleverland.local>
2018-03-22 10:30:23 +0100

    Add default configuration for camera source objects

---
+++ app/src/main/java/pl/edu/agh/sr/facecursor/utils/CameraSourceConfiguration.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.sr.facecursor.utils;
+
+import com.google.android.gms.vision.CameraSource;
+
+public class CameraSourceConfiguration {
+    public static final int PREVIEW_HEIGHT = 640;
+    public static final int PREVIEW_WIDTH = 480;
+
+    public static final int FACING = CameraSource.CAMERA_FACING_BACK;
+
+    public static final float REQUESTED_FPS = 30.0f;
+}
