9931e0c1440d28274829c2bd8a06039db555ad34
jsamol <julia.samol@gmail.com>
2018-03-22 17:07:23 +0100

    Add FaceUpdateHandler dependency to FaceTracker

--- app/src/main/java/pl/edu/agh/sr/facecursor/utils/facetracker/FaceTracker.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/utils/facetracker/FaceTracker.java
@@ -4,7 +4,16 @@ import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 
+import javax.inject.Inject;
+
 public class FaceTracker extends Tracker<Face> {
+
+    private FaceUpdateHandler mFaceUpdateHandler;
+
+    @Inject
+    public FaceTracker(FaceUpdateHandler mFaceUpdateHandler) {
+        this.mFaceUpdateHandler = mFaceUpdateHandler;
+    }
 
     @Override
     public void onNewItem(int i, Face face) {
--- app/src/main/java/pl/edu/agh/sr/facecursor/utils/facetracker/FaceTrackerFactory.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/utils/facetracker/FaceTrackerFactory.java
@@ -4,9 +4,13 @@ import com.google.android.gms.vision.MultiProcessor.Factory;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 
+import pl.edu.agh.sr.facecursor.dagger.facetracker.DaggerFaceTrackerComponent;
+
 public class FaceTrackerFactory implements Factory<Face> {
     @Override
     public Tracker<Face> create(Face face) {
-        return new FaceTracker();
+        return DaggerFaceTrackerComponent.builder()
+                .build()
+                .make();
     }
 }
