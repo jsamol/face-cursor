f8f5e4b2b59df6e3fdc54a8bda12a9c4f4587ab0
Julia Samól <juliasamol@Cleverland.local>
2018-03-22 10:18:42 +0100

    Create a package for ui components

--- app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/MainActivity.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package pl.edu.agh.sr.facecursor;
+package pl.edu.agh.sr.facecursor.ui.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pl.edu.agh.sr.facecursor.R;
 
 public class MainActivity extends AppCompatActivity {
 
