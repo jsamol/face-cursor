f5e413490a46a706682563e1c0e3669c2ce28d99
Julia Samól <juliasamol@Cleverland.local>
2018-03-22 10:42:41 +0100

    Add custom view for displaying image from the camera source

--- app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/MainActivity.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/MainActivity.java
@@ -2,12 +2,17 @@ package pl.edu.agh.sr.facecursor.ui.main;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
 import pl.edu.agh.sr.facecursor.R;
 import pl.edu.agh.sr.facecursor.dagger.main.DaggerMainComponent;
 import pl.edu.agh.sr.facecursor.dagger.main.MainModule;
 import pl.edu.agh.sr.facecursor.ui.BaseView;
+import pl.edu.agh.sr.facecursor.ui.main.layout.CameraSourceView;
 
 public class MainActivity extends BaseView implements IMainView {
+
+    @BindView(R.id.cameraSourceView)
+    CameraSourceView cameraSourceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/layout/CameraSourceView.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.sr.facecursor.ui.main.layout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class CameraSourceView extends ViewGroup {
+
+    private static final String TAG = "CameraSourceView";
+
+    public CameraSourceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
