adcb821af1e0bf6ffa2c6eaf783fe6ca4c084bb9
jsamol <julia.samol@gmail.com>
2018-03-22 15:23:09 +0100

    Add app configuration file

---
+++ app/src/main/java/pl/edu/agh/sr/facecursor/utils/Configuration.java
@@ -0,0 +1,8 @@
+package pl.edu.agh.sr.facecursor.utils;
+
+import android.Manifest;
+
+public class Configuration {
+    public static int CAMERA_PERMISSION_CODE = 1;
+    public static String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+}
