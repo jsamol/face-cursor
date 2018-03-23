50b60f626d4a241b9be28144a35ccb915171d9e8
Julia Samól <juliasamol@Cleverland.local>
2018-03-23 15:51:15 +0100

    CameraSourceView implementation

--- app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/layout/CameraSourceView.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/ui/main/layout/CameraSourceView.java
@@ -1,20 +1,109 @@
 package pl.edu.agh.sr.facecursor.ui.main.layout;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.AttributeSet;
+import android.view.SurfaceView;
 import android.view.ViewGroup;
+
+import com.google.android.gms.common.images.Size;
+import com.google.android.gms.vision.CameraSource;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import pl.edu.agh.sr.facecursor.utils.AppConfiguration;
 
 public class CameraSourceView extends ViewGroup {
 
     private static final String TAG = "CameraSourceView";
 
+    private Context mContext;
+
+    @Inject
+    SurfaceView surfaceView;
+
+    @Inject
+    SurfaceHolderCallback surfaceHolderCallback;
+
+    @Inject
+    CameraSource cameraSource;
+
+    private boolean isSurfaceAvailable;
+
     public CameraSourceView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        this.mContext = context;
+
+        if (surfaceView != null && surfaceHolderCallback != null) {
+            surfaceHolderCallback.bindCameraSourceView(this);
+            surfaceView.getHolder().addCallback(surfaceHolderCallback);
+            addView(surfaceView);
+        }
     }
 
-    //TODO: Implement the view to display image from the camera
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        final int layoutWidth = r - l;
+        final int layoutHeight = b - t;
 
+        int width = AppConfiguration.CAMERA_PREVIEW_DEFAULT_LANDSCAPE_WIDTH;
+        int height = AppConfiguration.CAMERA_PREVIEW_DEFAULT_LANDSCAPE_HEIGHT;
+        if (cameraSource != null) {
+            Size size = cameraSource.getPreviewSize();
+            if (size != null) {
+                width = size.getWidth();
+                height = size.getHeight();
+            }
+        }
+
+        if (isPortraitMode()) {
+            int swap = width;
+            width = height;
+            height = swap;
+        }
+
+        int childWidth = layoutWidth;
+        int childHeight = (int)(((float) layoutWidth / (float) width) * height);
+
+        if (childHeight > layoutHeight) {
+            childHeight = layoutHeight;
+            childWidth = (int)(((float) layoutHeight / (float) height) * width);
+        }
+
+        for (int i = 0; i < getChildCount(); i++) {
+            getChildAt(i).layout(0, 0, childWidth, childHeight);
+        }
+
+        try {
+            start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    void start() throws IOException {
+        if (cameraSource != null && isSurfaceAvailable) {
+            cameraSource.start(surfaceView.getHolder());
+        }
+    }
+
+    private boolean isPortraitMode() {
+        int orientation = mContext.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            return false;
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return true;
+        }
+
+        return false;
+    }
+
+    void setSurfaceAvailable(boolean surfaceAvailable) {
+        isSurfaceAvailable = surfaceAvailable;
     }
 }
