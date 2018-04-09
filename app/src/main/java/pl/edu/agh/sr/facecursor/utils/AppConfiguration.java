a708c1dca188fa0f4225b7072c4d437c8e565e94
Julia Samól <juliasamol@Cleverland.local>
2018-04-09 17:20:14 +0200

    Make all consts final

--- app/src/main/java/pl/edu/agh/sr/facecursor/utils/AppConfiguration.java
+++ app/src/main/java/pl/edu/agh/sr/facecursor/utils/AppConfiguration.java
@@ -3,9 +3,9 @@ package pl.edu.agh.sr.facecursor.utils;
 import android.Manifest;
 
 public class AppConfiguration {
-    public static int CAMERA_PERMISSION_CODE = 1;
-    public static String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
+    public static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
 
-    public static int CAMERA_PREVIEW_DEFAULT_LANDSCAPE_WIDTH = 320;
-    public static int CAMERA_PREVIEW_DEFAULT_LANDSCAPE_HEIGHT = 240;
+    public static final int CAMERA_PREVIEW_DEFAULT_LANDSCAPE_WIDTH = 320;
+    public static final int CAMERA_PREVIEW_DEFAULT_LANDSCAPE_HEIGHT = 240;
 }
